5cabdc2e83e0cd422f932934fef6f6ac80ed012e
chenxiaohua <3389162133@qq.com>
2023-12-12 11:17:01 +0800

    update .gitignore

--- python/out/prod/Tree/b_tree_node.py
+++
@@ -1,6 +0,0 @@
-class TreeNode():
-
-    def __init__(self, value=None,left=None,right=None) -> None:
-        self.value = value  # 值
-        self.left = left  # 左子树
-        self.right = right  # 右子树
